cc9f58b68a97251d13b210c3ec5b89834595ec4a
pierro145 <pierremunier@hotmail.fr>
2020-10-08 15:53:18 +0200

    update

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -35,7 +35,8 @@ else:
 
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost',
-                 '127.0.0.1', 'purbeurre75.herokuapp.com']
+                 '127.0.0.1', 'purbeurre75.herokuapp.com',
+                 '46.101.144.128']
 
 # auth
 
